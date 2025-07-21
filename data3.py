53157854a273697423bfaf649ec8f68da389ab64
seen8778 <85940512+seen8778@users.noreply.github.com>
2025-07-21 14:24:38 +0700

    Add files via upload

---
+++ data3.py
@@ -0,0 +1,30 @@
+i = 1
+while i <= 21:
+    print(i, end=',')
+    i += 2
+print()
+
+nums = [2, 5, 8, 11, 13, 14, 17]
+i = 0
+while i < len(nums):
+    print(nums[i], end=',')
+    i += 1
+print()
+
+i = 100
+while i >= -100:
+    print(i, end=',')
+    i -= 10
+print()
+
+i = -30
+while i <= 30:
+    print(i, end=',')
+    i += 10
+print()
+
+i = 15
+while i <= 55:
+    print(i, end=',')
+    i += 8
+print()
